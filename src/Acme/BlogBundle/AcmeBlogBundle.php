9415278f3bfb7e2905617cdb95f92a8507e8ca90
Yolan <contact.ybehlouli@gmail.com>
2015-01-08 17:32:59 +0100

    Ajout d'un Bungle Blog manuellement, définition des routages en cours

---
+++ src/Acme/BlogBundle/AcmeBlogBundle.php
@@ -0,0 +1,10 @@
+<?php
+// src/Acme/BlogBundle/AcmeBlogBundle.php
+namespace Acme\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeBlogBundle extends Bundle
+{
+	
+}
---
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+// src/Acme/BlogBundle/Controller/BlogController.php
+
+namespace Acme\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends Controller
+{
+    public function showAction()
+    {
+        die('here');
+	}
+	
+    public function pageAction()
+    {
+
+    }
+}
+
+

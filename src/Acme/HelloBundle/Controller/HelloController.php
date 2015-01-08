2b51549783eb800dd243b3335ffc91e59f61fa14
Yolan <contact.ybehlouli@gmail.com>
2015-01-08 11:14:04 +0100

    Fix du controller de /Hello

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -7,9 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HelloController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($foo, $bar)
     {
-        return $this->render('AcmeHelloBundle:Hello:index.html.twig', array('name' => $name));
+        return $this->render('AcmeHelloBundle:Hello:index.html.twig', array('foo' => $foo, 'bar' => $bar));
 
         // render a PHP template instead
         // return $this->render('AcmeHelloBundle:Hello:index.html.php', array('name' => $name));

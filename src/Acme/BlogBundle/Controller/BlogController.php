98e578935f1a9e244b3345fa574384377be56da7
Yolan <contact.ybehlouli@gmail.com>
2015-01-09 11:25:14 +0100

    Commit Temporaire - BlogBundle

--- src/Acme/BlogBundle/Controller/BlogController.php
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -8,14 +8,31 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
-    public function showAction()
-    {
-        die('here');
-	}
-	
-    public function pageAction()
-    {
+    public function showAction($slug)
+    { # route acme_blog_show
 
+        // traitement sur la variable $slug i.e base de donnée
+        $slug = 'Vous êtes sur l\'entrée '.$slug.' du blog :)';
+
+
+        return $this->render('AcmeBlogBundle:Blog:show.html.twig',
+                        array(  'slug' => $slug,
+                    ));
+	}
+    
+    public function showRedirectAction($slug)
+    { # route acme_blog_showRedirect
+        
+    }
+    
+    public function pageAction()
+    { # route acme_pageRedirect_showRedirect
+        
+    }
+    
+    public function pageRedirectAction()
+    { # route acme_page_showRedirect
+        
     }
 }
 

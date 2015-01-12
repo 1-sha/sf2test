2a4ecef6cb7491b9c5dc515a4dc225997f1b9809
Yolan <contact.ybehlouli@gmail.com>
2015-01-12 09:20:34 +0100

    Création automatique d'un Bundle Store

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,9 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            //My Bundless
             new Acme\HelloBundle\AcmeHelloBundle(),
             new Acme\TestBundle\AcmeTestBundle(),
             new Acme\BlogBundle\AcmeBlogBundle(),
+            new Acme\StoreBundle\AcmeStoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
